bd4674d24b92f3da37b554304b7b9235c7e500bf
Oluwatobi Ajide <biajide@gmail.com>
2020-11-20 21:03:38 -0500

    xc

--- server.py
+++ server.py
@@ -1,31 +1,43 @@
-
+import re
 import tweepy
 import pandas as pd
 import time
-consumer_key = "q2odiAzgwoTWodRDtKa6pRqtI"
-consumer_secret = "zNQ8SZH0oEVEprQlGNF0BtKrluTYUG1KkautdmkJeWvTgzPMU3"
-access_token = "3413666200-OTGCcLNnd7miQnL60IibnddRLCgsfMXaKfeCgnW"
-access_token_secret = "Tz45UeQam241P8oq9OSJLaw0r1NQiNwB7wET0EZqCpAOF"
+
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
 
 def search(text_query):
-    text_query = 'Could you please make a free live streaming of the Thanksgiving Day Parade available to all Peacock account holders?'
     count = 150
     try:
         # Creation of query method using parameters
         tweets = tweepy.Cursor(api.search, q=text_query).items(count)
+        tweets = filter(lambda x: 'RT @' not in x.text, tweets) 
 
     # Pulling information from tweets iterable object
-        tweets_list = [[tweet.created_at, tweet.id, tweet.text]
+        tweets_list = [[tweet.created_at, tweet.id, tweet.text, tweet.user.screen_name]
                        for tweet in tweets]
 
     # Creation of dataframe from tweets list
-        tweets_df = pd.DataFrame(tweets_list, columns=['Created At', 'Tweet Id', 'Tweet Text'])
-
+        tweets_df = pd.DataFrame(tweets_list, columns=['Created At', 'Tweet Id', 'Tweet Text', "User Id"])
+        return tweets_df
     except BaseException as e:
         print('failed on_status,', str(e))
         time.sleep(3)
 
+
+def clean_tweet(tweet):
+        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) |(\w+:\/\/\S+)", " ", tweet).split())
+    
+def timeline(count):
+    # fetching the statuses 
+    statuses = api.home_timeline(count=count) 
+  
+# printing the screen names of each status 
+    for status in statuses:
+        result = search(status.text)
+        if (not result.empty):
+          print(result)
+
+timeline(25)
